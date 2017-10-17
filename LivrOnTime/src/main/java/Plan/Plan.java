a7cacde3deb3c4b5eac89c8d021a8d460dc85d4b
mtp24 <matpyu@orange.fr>
2017-10-17 10:39:42 +0200

    Add files via upload

--- LivrOnTime/src/main/java/Plan/Plan.java
+++ LivrOnTime/src/main/java/Plan/Plan.java
@@ -7,7 +7,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 public class Plan {
-	public static HashMap<long[],Intersection> Intersections = new HashMap();
+	public static HashMap<Long,Intersection> Intersections = new HashMap();
 	public static ArrayList<Troncon> Troncons = new ArrayList<Troncon>();
 	
 
@@ -18,7 +18,7 @@ public void CreerIntersections(NodeList noeuds) {
 	  final int nbNoeuds = noeuds.getLength();
       for(int j = 0; j<nbNoeuds; j++) {
           final Element noeud = (Element) noeuds.item(j);
-         long k = Integer.parseInt((String) noeud.getAttribute("id"));
+         long k = Long.parseLong((String) noeud.getAttribute("id"));
           Intersection intersection = new Intersection( k , Integer.parseInt(noeud.getAttribute("x")) , Integer.parseInt(noeud.getAttribute("y")));
           Intersections.put(k,intersection);
       }  
@@ -27,9 +27,9 @@ public void CreerTroncons(NodeList troncons) {
   final int nbTroncons = troncons.getLength();
   for(int j = 0; j<nbTroncons; j++) {
       final Element troncon = (Element) troncons.item(j);
-      int k = Integer.parseInt(troncon.getAttribute("destination"));
+      Long k = Long.parseLong(troncon.getAttribute("destination"));
       Intersection intersectionD = Plan.Intersections.get(k);
-      k = Integer.parseInt(troncon.getAttribute("origine"));
+      k = Long.parseLong(troncon.getAttribute("origine"));
       Intersection intersectionO = Plan.Intersections.get(k); 
       Troncons.add(new Troncon(intersectionD, Double.parseDouble(troncon.getAttribute("longueur")), troncon.getAttribute("nomRue") , intersectionO));
   
