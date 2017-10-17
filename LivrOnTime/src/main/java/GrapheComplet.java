7b859aaa045104de2faeff380325fbcfac436d50
lauriesordillon <lauriesordillon@10.43.10.72>
2017-10-17 11:25:40 +0200

    work on GrapheComplet

--- LivrOnTime/src/main/java/GrapheComplet.java
+++ LivrOnTime/src/main/java/GrapheComplet.java
@@ -1,9 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import Plan.Intersection;
 
 public class GrapheComplet {
+	public int [] duree;
+	public int [][] cout;
+	public int []equivalent;
 	
-	// blablabla
-	public int test() {
-		return 0;
+	public GrapheComplet (HashMap<Long,Intersection> intersections) {
+		
+		equivalent=new int[intersections.size()];// Etape de création d'équivalent. cera présent dans Plan, a supprimer
+		int iterator=0;
+		for (Map.Entry key : intersections.entrySet()) {
+			equivalent[iterator]=(Integer) key.getKey();
+			iterator++;
+		}
+		
+		//construction de durée
+		
+	}
+	
+	//On peut renvoyer des tableaux ?
+	public int[][] getCout () {
+		return cout;
+	}
+	public int[] getDuree () {
+		return duree;
 	}
 
 }
