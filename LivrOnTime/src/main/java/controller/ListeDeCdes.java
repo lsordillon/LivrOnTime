0bad4b761f5db7f6b6d4e08c6e37dc6283f76411
vidya <vidya-imen@hotmail.fr>
2017-11-01 17:01:33 +0100

    liste de commande

---
+++ LivrOnTime/src/main/java/controller/ListeDeCdes.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.util.LinkedList;
+
+
+
+public class ListeDeCdes {
+	
+	
+	private LinkedList<Commande> liste;
+	private int indiceCrt;
+	
+	public ListeDeCdes(){
+		indiceCrt = -1;
+		liste = new LinkedList<Commande>();
+	}
+	
+	public void ajoute(Commande c){
+        int i = indiceCrt+1;
+        while(i<liste.size()){
+            liste.remove(i);
+        }
+       // indiceCrt++;
+        liste.add(indiceCrt, c);
+    }
+	
+	
+	public void undo(){
+		if (indiceCrt >= 0){
+			Commande cde = liste.get(indiceCrt);
+			indiceCrt--;
+			cde.undoCde();
+		}
+	}
+	
+	
+	public void annule(){
+		if (indiceCrt >= 0){
+			Commande cde = liste.get(indiceCrt);
+			liste.remove(indiceCrt);
+			indiceCrt--;
+		}
+	}
+
+	
+	public void redo(){
+		if (indiceCrt < liste.size()-1){
+			indiceCrt++;
+			Commande cde = liste.get(indiceCrt);
+			cde.redoCde();
+		}
+	}
+	
+	//ce fait à chaque ouverture du plan
+	   public void reset(){
+	        indiceCrt = -1;
+	        liste.clear();  
+	    }
+
+}
