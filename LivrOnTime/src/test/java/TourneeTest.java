0f317b9b240301fcd28c5182ca82343d620336e2
mtp24 <matpyu@orange.fr>
2017-11-10 19:13:27 +0100

    test u

---
+++ LivrOnTime/src/test/java/TourneeTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+
+import util.tsp.IteratorMinFirst;
+
+public class TourneeTest {
+
+	@Test
+	public void testIterator() {
+		
+		
+	}
+}
